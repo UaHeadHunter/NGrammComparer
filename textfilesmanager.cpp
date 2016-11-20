c769636d591ed1e039ae70d0c87b9d5f72c13661
UaHeadHunter <ua.head.hunterr@gmail.com>
2016-11-20 15:31:44 +0200

    Added ngramm count.

--- textfilesmanager.cpp
+++ textfilesmanager.cpp
@@ -11,6 +11,8 @@ TextFilesManager::TextFilesManager(QObject *parent) : QObject(parent)
 void TextFilesManager::slotTextFilesLoaded(const QFileInfoList &fileNames, int nGrammSize, int mode,
                                            bool includeNumbers)
 {
+    int textCount = 0;
+    QHash<QString, int> dictionary;
     for (const QFileInfo& fileName: fileNames)
     {
         QFile file(fileName.filePath());
@@ -19,13 +21,36 @@ void TextFilesManager::slotTextFilesLoaded(const QFileInfoList &fileNames, int n
             QString text = file.readAll();
             TextParser parser(mode == Letters, includeNumbers);
 
-            for (int i = 0 ; i < text.length(); ++i)
+            for (int i = 0 ; i < text.length() - nGrammSize; ++i)
             {
-                if (parser.isSymbolAllowed(text.at(i)))
+                QString nGramm;
+                for (int n = i; nGramm.count() != nGrammSize; )
                 {
-                    qDebug() << text.at(i);
+                    QChar symbol = text.at(n).toLower();
+                    if (parser.isSymbolAllowed(symbol))
+                    {
+                        nGramm.append(symbol);
+                        ++n;
+                    }
+                    else
+                    {
+                        text.remove(n, 1);
+                    }
+                }
+
+                qDebug() << nGramm;
+
+                if (dictionary.contains(nGramm))
+                {
+                    ++dictionary[nGramm];
+                }
+                else
+                {
+                    dictionary.insert(nGramm, 1);
                 }
             }
         }
+        ++textCount;
     }
+    qDebug() << dictionary;
 }
