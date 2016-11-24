8d69cad387daf4b5cbd2a0ee8de2d05e60d6988c
UaHeadHunter <ua.head.hunterr@gmail.com>
2016-11-24 23:56:30 +0200

    Added all needed methods. Added modes. Fixed wrong data calculation.

--- textmodel.cpp
+++ textmodel.cpp
@@ -1,33 +1,123 @@
 #include "textmodel.h"
 #include "textitem.h"
 
-TextModel::TextModel(const QStringList &items, QAbstractTableModel *parent)
-    : QAbstractTableModel(parent)
+#include <QVariant>
+
+TextModel::TextModel(QObject *parent)
+    : QAbstractTableModel(parent),
+      mMode(AbsoluteFrequency),
+      mCount(0)
 {
+}
+
+int TextModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return mData.count();
+}
+
+int TextModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return mCount + 1;
+}
+
+QVariant TextModel::data(const QModelIndex &index, int role) const
+{
+    QVariant rValue;
+
+    if (index.row() < 0 || index.row() >= mData.count() || role != Qt::DisplayRole) {
+        return QVariant();
+    }
+    int column = index.column();
+    int row = index.row();
+
+    if (column == 0)
+    {
+        rValue = mData.keys().at(index.row());
+    }
+    else if (column > 0 && column <= mData.values().at(row).count())
+    {
+        TextItem *item = mData.values().at(row).at(column - 1);
+        switch (static_cast<DisplayMode>(mMode))
+        {
+        case AbsoluteFrequency:
+            rValue = item->mAbsFreq;
+            break;
+        case RelativeFrequency:
+            rValue = item->mRelFreq;
+            break;
+        case DeviationFromAbsoluteFrequency:
+            rValue = item->mDevAbsFreq;
+            break;
+        case DeviationFromRelativeFrequency:
+            rValue = item->mDevRelFreq;
+            break;
+        }
+
+    }
+
+    return rValue;
+}
+
+void TextModel::slotAddWords(const QStringList &items)
+{
+    beginInsertRows(index(0, 0), 0, items.count() - 1);
     for (const QString &item : items)
     {
         mData.insert(item, QList<TextItem*>());
     }
+    endInsertRows();
 }
 
 void TextModel::slotBuildModel(QList<QHash<QString, int> > texts)
 {
-    for (int i = 0 ; i < texts.length(); ++i)
+    beginInsertRows(index(0, 0), 0, texts.count() - 1);
+    mCount = texts.count();
+    for (QHash<QString, int> &text : texts)
     {
-        QHash<QString, int> text = texts.value(i);
         for (const QString &word : text.keys())
         {
             int absFreq = text.value(word);
-            double relFreq = text.value(word) / mData.count();
+            double relFreq = text.value(word) / (double)mData.count();
             double devAbsFreq = absFreq;
             double devRelFreq = relFreq;
             QList<TextItem*> list = mData.value(word);
-            if (list.count() > 1)
+
+            if (list.count() > 0)
             {
-                devAbsFreq = list.first()->mAbsFreq - absFreq;
-                devRelFreq = list.first()->mRelFreq - relFreq;
+                devAbsFreq = absFreq - list.first()->mAbsFreq;
+                devRelFreq = relFreq - list.first()->mRelFreq;
             }
-            mData.value(word).append(new TextItem(absFreq, relFreq, devAbsFreq, devRelFreq));
+            mData[word].append(new TextItem(absFreq, relFreq, devAbsFreq, devRelFreq));
         }
     }
+    endInsertRows();
+    QVector<int> roles;
+    roles.append(Qt::DisplayRole);
+    emit dataChanged(index(0,0), index(mData.count() - 1, mData.values().at(0).count() - 1),
+                     roles);
+}
+
+void TextModel::slotClear()
+{
+    beginResetModel();
+
+    for (QList<TextItem*> item : mData.values())
+    {
+        qDeleteAll(item);
+    }
+    mData.clear();
+
+    endResetModel();
+}
+
+int TextModel::mode() const
+{
+    return mMode;
+}
+
+void TextModel::setMode(int mode)
+{
+    mMode = mode;
 }
--- textmodel.h
+++ textmodel.h
@@ -3,21 +3,37 @@
 
 #include <QAbstractTableModel>
 
-class TextItem;
+struct TextItem;
 class TextModel : public QAbstractTableModel
 {
     Q_OBJECT
 public:
-    explicit TextModel(const QStringList& items, QAbstractTableModel *parent = 0);
+    enum DisplayMode
+    {
+        AbsoluteFrequency = 0,
+        RelativeFrequency,
+        DeviationFromAbsoluteFrequency,
+        DeviationFromRelativeFrequency
+    };
+
+    explicit TextModel(QObject *parent = 0);
+
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    QVariant data(const QModelIndex &index, int role) const;
+
+    int mode() const;
+    void setMode(int mode);
 
 public slots:
+    void slotAddWords(const QStringList& items);
     void slotBuildModel(QList<QHash<QString, int>> texts);
-
-private:
-    void
+    void slotClear();
 
 private:
     QMap<QString, QList<TextItem*>> mData;
+    int mMode;
+    int mCount;
 };
 
 #endif // TEXTMODEL_H
